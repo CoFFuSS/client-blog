be468f1bfb374c6812858a6335ee402838556832
Danila <danikk066@gmail.com>
2024-05-12 21:09:45 +0300

    fix: attemp to fix vercel deploy

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -1,6 +1,12 @@
+import { unstable_setRequestLocale } from 'next-intl/server';
+
+import { ParamsLocale } from '@/types/common';
+
 import styles from './page.module.scss';
 
-export default function Home() {
+export default function Home({ params: { locale } }: ParamsLocale) {
+  unstable_setRequestLocale(locale);
+
   return (
     <main className={styles.main}>
       <div>
