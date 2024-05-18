23d0c74ba3fc73a2fcae7691f9f2cab8c69a0f82
Danila <danikk066@gmail.com>
2024-05-19 00:48:21 +0300

    feat: added and styled about-us component

---
+++ src/components/Backstory/index.tsx
@@ -0,0 +1,43 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { useTranslations } from 'next-intl';
+
+import background from '@/assets/images/backstory-background.png';
+import { routes } from '@/constants/routes';
+
+import styles from './styled.module.scss';
+
+import Button from '../Button';
+
+export default function Backstory() {
+  const t = useTranslations('home.backstory');
+
+  return (
+    <section className={styles.container}>
+      <Image
+        className={styles.image}
+        src={background}
+        alt='backstory-background'
+      />
+      <div className={styles.content}>
+        <h5 className={styles.subtitle}>{t('subtitle')}</h5>
+        <h1 className={styles.title}>{t('title')}</h1>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip.
+        </p>
+        <Button
+          variant='primary'
+          dataCy='about-us-button'
+        >
+          <h5>
+            <Link href={routes.about}>
+              {t('button')} <span>{'>'}</span>
+            </Link>
+          </h5>
+        </Button>
+      </div>
+    </section>
+  );
+}
--- src/constants/componentToShow.ts
+++ src/constants/componentToShow.ts
@@ -1,4 +1,5 @@
 import About from '@/components/About';
+import Backstory from '@/components/Backstory';
 import ChooseCategory from '@/components/ChooseCategory';
 import Posts from '@/components/Posts';
 
@@ -6,4 +7,5 @@ export const componentsToShow = [
   { component: Posts, id: 1 },
   { component: About, id: 2 },
   { component: ChooseCategory, id: 3 },
+  { component: Backstory, id: 4 },
 ];
