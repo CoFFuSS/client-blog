786fccbecf9baf5946dd8e6656015ae8c2992cd4
Danila <danikk066@gmail.com>
2024-05-19 19:51:34 +0300

    feat: added and styled slider for testimonials

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,8 +1,11 @@
 'use client';
 
 import { useState } from 'react';
+import Image from 'next/image';
 
 import { testimonials } from '@/constants/testimonials';
+
+import styles from './styled.module.scss';
 
 import { LeftArrow } from '../Icons/LeftArrow';
 import { RightArrow } from '../Icons/RightArrow';
@@ -18,10 +21,36 @@ export default function Slider() {
     setActiveSlide((prevSlide) => (prevSlide - 1 + testimonials.length) % testimonials.length);
   };
 
+  const currentTransform = -activeSlide * 100;
+
   return (
-    <div>
-      <h3>Slider</h3>
-      <div>
+    <div className={styles.slider}>
+      <div
+        className={styles.slider__items}
+        style={{ transform: `translateX(${currentTransform}%)` }}
+      >
+        {testimonials.map(({ authorName, location, photo, text, id }, index) => (
+          <div
+            key={id}
+            className={`${styles.slider__item} ${index === activeSlide ? styles.active : ''}`}
+          >
+            <p className={styles.text}>{text}</p>
+            <div className={styles.author__row}>
+              <Image
+                src={photo}
+                alt={authorName}
+                width={48}
+                height={48}
+              />
+              <div>
+                <p className={styles.author__name}>{authorName}</p>
+                <p className={styles.author__location}>{location}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className={styles.arrows}>
         {activeSlide > 0 && <LeftArrow onClick={handlePrev} />}
         {activeSlide < testimonials.length - 1 && <RightArrow onClick={handleNext} />}
       </div>
