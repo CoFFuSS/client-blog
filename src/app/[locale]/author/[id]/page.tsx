fd7a7eb8c95533a2afbc622d2b3bf1bf84edcb7b
Danila <danikk066@gmail.com>
2024-05-23 00:35:42 +0300

    feat: added and styled author-page, blog-post-card

--- src/app/[locale]/author/[id]/page.tsx
+++ src/app/[locale]/author/[id]/page.tsx
@@ -1,11 +1,66 @@
+import Image from 'next/image';
+import { useTranslations } from 'next-intl';
+
+import SocialWrapper from '@/components/SocialWrapper';
+import { authors } from '@/constants/authors';
+import { BlogPostFields, blogPosts } from '@/constants/blogPosts';
+import BlogPostCard from '@/components/BlogPostCard';
+
+import styles from './styles.module.scss';
+
 interface AuthorProps {
   params: { id: string };
 }
 
 export default function Authors({ params: { id } }: AuthorProps) {
+  const t = useTranslations('author');
+  const currentAuthor = authors.find(({ id: currentId }) => currentId === +id);
+
+  if (!currentAuthor) {
+    return (
+      <h1>
+        {t('noAuthor')} {id}
+      </h1>
+    );
+  }
+
+  const { avatar, name } = currentAuthor;
+  const authorPosts: BlogPostFields[] = blogPosts.filter(({ author }) => author === name);
+
+  if (authorPosts && authorPosts.length === 0) {
+    return <h1>{t('noAuthorPosts')}</h1>;
+  }
+
   return (
-    <div>
-      <h4>Author{id}</h4>
-    </div>
+    <main className={styles.container}>
+      <div className={styles.profile}>
+        <div>
+          <Image
+            src={avatar}
+            alt={name}
+          />
+        </div>
+        <div className={styles.profile__upper}>
+          <h1>{t('title', { name })}</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Non blandit massa enim nec. Scelerisque
+            viverra mauris in aliquam sem. At risus viverra adipiscing at in tellus.
+          </p>
+          <SocialWrapper />
+        </div>
+      </div>
+      <div className={styles.posts}>
+        <h1>{t('postTitle')}</h1>
+        <div className={styles.posts__list}>
+          {authorPosts.map((post: BlogPostFields) => (
+            <BlogPostCard
+              key={post.id}
+              {...post}
+            />
+          ))}
+        </div>
+      </div>
+    </main>
   );
 }
--- src/constants/blogPosts.ts
+++ src/constants/blogPosts.ts
@@ -1,9 +1,24 @@
+import { StaticImageData } from 'next/image';
+
 import icon from '@/assets/icons/shuttle.svg';
 import postImage1 from '@/assets/images/post-image-1.webp';
 
 import { tags } from './tags';
 
-export const blogPosts = [
+export interface BlogPostFields {
+  image: StaticImageData;
+  category: string;
+  title: string;
+  text: string;
+  date: string;
+  author: string;
+  content: { title: string; text: string; points: string[] };
+  tags: string[];
+  icon: string;
+  id: number;
+}
+
+export const blogPosts: BlogPostFields[] = [
   {
     image: postImage1,
     category: 'Startup',
@@ -23,5 +38,25 @@ export const blogPosts = [
     tags: [tags.Life],
     icon,
     id: 1,
+  },
+  {
+    image: postImage1,
+    category: 'Startup',
+    title: 'Design tips for designers that cover everything you need',
+    text: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.',
+    date: '27th January, 2023',
+    author: 'Floyd Miles',
+    content: {
+      title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non blandit massa enim nec. Scelerisque viverra mauris in aliquam sem. At risus viverra adipiscing at in tellus. Sociis natoque penatibus et magnis dis parturient montes. Ridiculus mus mauris vitae ultricies leo. Neque egestas congue quisque egestas diam. Risus in hendrerit gravida rutrum quisque non.',
+      points: [
+        'Lorem ipsum dolor sit amet',
+        'Non blandit massa enim nec scelerisque',
+        'Neque egestas congue quisque egestas',
+      ],
+    },
+    tags: [tags.Life],
+    icon,
+    id: 2,
   },
 ];
